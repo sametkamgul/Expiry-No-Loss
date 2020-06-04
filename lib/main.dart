e1a6661e3378483c49d25077c397f448e6afd237
sametmate <samet@mate.com.tr>
2020-06-04 03:38:04 +0300

    item widget and bottom tab menu is seperated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'widgets/item.dart';
+import 'widgets/bottomTab.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,8 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // full screen width and height
     double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
+    //double screenHeight = MediaQuery.of(context).size.height;
+    
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -53,370 +55,32 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            //Container(),
+            Container(),
             //Expanded(child: Text('tetet')),
-            ListView(
-              shrinkWrap: true,
-              physics: ScrollPhysics(),
-              children: <Widget>[
+            
 
-                //item
-                Padding(padding: EdgeInsets.only(top: 5.0)),
-                GestureDetector(
-                  onTap: () {
-                    print("item-1");
-                  },
-                  child: new Container(
-                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                    height: 75.0,
-                    color: Colors.transparent,
-                    child: new Container(
-                      decoration: new BoxDecoration(
-                        color: Color(0xFFC88264),
-                        borderRadius: new BorderRadius.all(
-                        const Radius.circular(40.0),
-                        )
-                      ),
-                      child: new Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Orange',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Text(
-                                  '03/06/2020',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                  ),
-                                ),
-                                Text(
-                                  '1 day left',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
-                ),
 
-                Padding(padding: EdgeInsets.only(top: 5.0)),
-                // item-2
-                GestureDetector(
-                  onTap: () {
-                    print('item-2');
-                  },
-                  onLongPress: () {
-                    print('editing');
-                  },
-                  child: new Container(
-                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                    height: 75.0,
-                    color: Colors.transparent,
-                    child: new Container(
-                      decoration: new BoxDecoration(
-                        color: Color(0xFFC88264),
-                        borderRadius: new BorderRadius.all(
-                        const Radius.circular(40.0),
-                        )
-                      ),
-                      child: new Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Milk',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: <Widget>[
-                                Text(
-                                  '03/06/2020',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                  ),
-                                ),
-                                Text(
-                                  '1 day left',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
-                ),
-              ],
+            Expanded(
+              child: ListView(      
+                //shrinkWrap: true,          
+                children: <Widget>[
+                  ItemWidget(itemType: 'expiry', itemName: 'Portakal', itemDateTime: '04/06/2020', itemCountdown: 50),
+                  ItemWidget(itemType: 'expiry', itemName: 'Elma', itemDateTime: '05/06/2020', itemCountdown: 10),
+                  ItemWidget(itemType: 'to-buy', itemName: 'Süt', itemDateTime: '06/08/2020', itemCountdown: 5),
+                  ItemWidget(itemType: 'to-do', itemName: 'Domates', itemDateTime: '24/07/2020', itemCountdown: 15),
+                  ItemWidget(itemType: 'expiry', itemName: 'Domates', itemDateTime: '24/07/2020', itemCountdown: 15),
+                  ItemWidget(itemType: 'to-do', itemName: 'Domates', itemDateTime: '24/07/2020', itemCountdown: 15),
+                  ItemWidget(itemType: 'to-buy', itemName: 'Domates', itemDateTime: '24/07/2020', itemCountdown: 15),
+                  ItemWidget(itemType: 'expiry', itemName: 'Domates', itemDateTime: '24/07/2020', itemCountdown: 15),
+                  ItemWidget(itemType: 'stock', itemName: 'Yoğurt', itemDateTime: '24/07/2020', itemCountdown: 15),
+                ],
+              ),
             ),
 
-/* 
-              ListView(
-              shrinkWrap: true,
-              physics: ScrollPhysics(),
-                children: <Widget>[
-                  
-                ],
-              ), */
-
-              // bottom menu
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-
-                    // Expiry-menu item
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: new AnimatedContainer(
-                        duration: Duration(milliseconds: 100),
-                        curve: Curves.bounceInOut,
-                        padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                        height: !_biggerExpiry ? 48.0 : 96.0,
-                        width: screenWidth,
-                        color: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: () {
-                            print('bigger-expiry');
-                            setState(() {
-                              _biggerExpiry = !_biggerExpiry;
-                              _biggerStock = false;
-                              _biggerToDo = false;
-                              _biggerToBuy = false;
-                            });
-                          },
-                          child: new Container(
-                            width: 4.0 * screenWidth / 4.0,
-                            decoration: new BoxDecoration(
-                              color: Color(0xFFC88264),
-                              borderRadius: new BorderRadius.only(
-                                topLeft: Radius.circular(40.0),
-                              )
-                            ),
-                            child: Align(
-                              alignment: Alignment.centerLeft,                              
-                              child: Container(
-                                child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      width: screenWidth / 4.0,
-                                      child: Align(
-                                        alignment: Alignment.center ,
-                                        child: Text(
-                                          'expiry',
-                                          style: TextStyle(
-                                            fontSize: !_biggerExpiry ? 18.0 : 28.0,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    // Stock-menu item
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: new AnimatedContainer(
-                        duration: Duration(milliseconds: 100),
-                        curve: Curves.bounceInOut,
-                        padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                        height: !_biggerStock ? 48.0 : 96.0,
-                        width: 3.0 * screenWidth / 4.0,
-                        color: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: () {
-                            print('bigger_stock');
-                            setState(() {
-                              _biggerExpiry = false;
-                              _biggerStock = !_biggerStock;
-                              _biggerToDo = false;
-                              _biggerToBuy = false;
-                            });
-                          },
-                          child: new Container(
-                            width: screenWidth / 4.0,
-                            decoration: new BoxDecoration(
-                              color: Color(0xFF6B75E3),
-                              borderRadius: new BorderRadius.only(
-                                topLeft: const Radius.circular(40.0),
-                              )
-                            ),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Container(
-                                child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      width: screenWidth / 4.0,
-                                      child: Align(
-                                        alignment: Alignment.center,
-                                        child: Text(
-                                          'stock',
-                                          style: TextStyle(
-                                            fontSize: !_biggerStock ? 18.0 : 28.0,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    //  to-do-menu item
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: new AnimatedContainer(
-                        duration: Duration(milliseconds: 100),
-                        curve: Curves.bounceInOut,
-                        padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                        height: !_biggerToDo ? 48.0 : 96.0,
-                        width: 2.0 * screenWidth / 4.0,
-                        color: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: () {
-                            print('bigger_todo');
-                            setState(() {
-                              _biggerExpiry = false;
-                              _biggerStock = false;
-                              _biggerToDo = !_biggerToDo;
-                              _biggerToBuy = false;
-
-                            });
-                          },
-                          child: new Container(
-                            width: screenWidth / 4.0,
-                            decoration: new BoxDecoration(
-                              color: Color(0xFF7BB076),
-                              borderRadius: new BorderRadius.only(
-                                topLeft: const Radius.circular(40.0),
-                              )
-                            ),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Container(
-                                child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      width: screenWidth / 4.0,
-                                      child: Align(
-                                        alignment: Alignment.center,
-                                        child: Text(
-                                          'to-do',
-                                          style: TextStyle(
-                                            fontSize: !_biggerToDo ? 18.0 : 28.0,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    //  to-buy-menu item
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: new AnimatedContainer(
-                        duration: Duration(milliseconds: 100),
-                        curve: Curves.bounceInOut,
-                        padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                        height: !_biggerToBuy ? 48.0 : 96.0,
-                        width: 1.0 * screenWidth / 4.0,
-                        color: Colors.transparent,
-                        child: GestureDetector(
-                          onTap: () {
-                            print('bigger_tobuy');
-                            setState(() {
-                              _biggerExpiry = false;
-                              _biggerStock = false;
-                              _biggerToDo = false;
-                              _biggerToBuy = !_biggerToBuy;
-                            });
-                          },
-                          child: new Container(
-                            width: screenWidth / 4.0,
-                            decoration: new BoxDecoration(
-                              color: Color(0xFFD86579),
-                              borderRadius: new BorderRadius.only(
-                                topLeft: const Radius.circular(40.0),
-                              )
-                            ),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Container(
-                                child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      width: screenWidth / 4.0,
-                                      child: Align(
-                                        alignment: Alignment.center,
-                                        child: Text(
-                                          'to-buy',
-                                          style: TextStyle(
-                                            fontSize: !_biggerToBuy ? 18.0 : 28.0,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                  ],
-                )
-              ),
+            // Bottom Tab Menu
+            BottomTab(tab1: 'expiry', tab2: 'stock', tab3: 'to-do', tab4: 'to-buy'),
           ],
         ),
       ),
---
+++ lib/widgets/item.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final String itemType;
+  final String itemName;
+  final String itemDateTime;
+  final int itemCountdown;
+  ItemWidget(
+    {
+      @required this.itemType,
+      @required this.itemName,
+      @required this.itemDateTime,
+      @required this.itemCountdown,
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 5.0),
+      child: GestureDetector(
+        onTap: () {
+          print(itemType + ', ' + itemName + ', ' + itemDateTime + ', ' + itemCountdown.toString());
+        },
+        child: new Container(
+          padding: EdgeInsets.only(left: 20.0, right: 20.0),
+          height: 75.0,
+          color: Colors.transparent,
+          child: new Container(
+            decoration: new BoxDecoration(
+              color: itemType == 'expiry' ? Color(0xFFC88264)
+                   : itemType == 'stock' ? Color(0xFF6B75E3)
+                   : itemType == 'to-do' ? Color(0xFF7BB076)
+                   : itemType == 'to-buy' ? Color(0xFFD86579)
+                   : Colors.white,
+              //color: Color(0xFFC88264),
+              border: Border.all(
+                width: 3,
+              ),
+              borderRadius: new BorderRadius.all(
+                const Radius.circular(40.0),
+                
+              )
+            ),
+            child: new Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    itemName,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(
+                        itemDateTime,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                      Text(
+                        itemCountdown.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ),
+        ),
+      ),
+    );
+
+  }
+}
